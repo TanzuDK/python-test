be3f36e1c266767fbd088a356dce0144d287ff7b
Robert Jensen <robert@robert-jensen.dk>
2022-07-29 14:12:46 +0200

    Cleanup ap

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title("Uber pickups in NYC for ops2devops")
+st.title("Uber pickups in NYC")
 st.subheader("Build using Tanzu Application Platform v1.2")
 
 DATE_COLUMN = "date/time"
