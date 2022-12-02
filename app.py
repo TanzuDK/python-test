352d0ca67474b81df2252c6ec40ad6ed82d031b0
Robert Jensen <robert@robert-jensen.dk>
2022-12-02 12:25:59 +0100

    updated for tam's

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import streamlit as st
 
 st.title("Uber pickups in NYC")
-st.subheader("Build using Tanzu Application Platform v1.3.2 with https")
+st.subheader("Build using Tanzu Application Platform v1.3.2 for the DK TAM team")
 
 DATE_COLUMN = "date/time"
 DATA_URL = (
