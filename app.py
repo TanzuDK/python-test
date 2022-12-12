aa583a81888a2dd7f10a68e11e70af8c6bfb73ab
Robert Jensen <robert@robert-jensen.dk>
2022-12-12 13:36:03 +0100

    kongsberg

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import streamlit as st
 
 st.title("Uber pickups in NYC")
-st.subheader("Build using Tanzu Application Platform v1.3.2 - test1")
+st.subheader("Build using Tanzu Application Platform v1.3.2 - kongsberg")
 
 DATE_COLUMN = "date/time"
 DATA_URL = (
