6a2b75ba646cd4e57ddae106c4f8785ac22736a1
Robert Jensen <robert@robert-jensen.dk>
2023-05-08 09:33:37 +0200

    added apptest

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import streamlit as st
 
 st.title("Uber pickups in NYC")
-st.subheader("Build using Tanzu Application Platform v1.3.2 - DK's super sælgere")
+st.subheader("Build using Tanzu Application Platform v1.3.2 - AppTest")
 
 DATE_COLUMN = "date/time"
 DATA_URL = (
